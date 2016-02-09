513aa3d634f0a5eb053cdef6370837b9a1806727
ZanJereb <zan@localhost.dlabs.lan>
2016-02-09 12:38:03 +0100

    initial commit

---
+++ Testing/Testing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Testing
+//
+//  Created by Zan on 2/9/16.
+//  Copyright © 2016 Zan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Testing/Testing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Testing
+//
+//  Created by Zan on 2/9/16.
+//  Copyright © 2016 Zan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
