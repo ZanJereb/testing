feaec4c89f4b911aefcbd04c37b1a33d9f35d69d
ZanJereb <zan@localhost.dlabs.lan>
2016-02-09 14:55:46 +0100

    Start building my cast screen #5

--- Testing/Testing/UserInterface/MyCasts/MyCastsViewController.m
+++ Testing/Testing/UserInterface/MyCasts/MyCastsViewController.m
@@ -8,7 +8,10 @@
 
 #import "MyCastsViewController.h"
 
-@interface MyCastsViewController ()
+@interface MyCastsViewController ()<UITableViewDataSource, UITableViewDelegate>
+
+@property(weak, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic) NSArray *items;
 
 @end
 
@@ -16,7 +19,16 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    
+    NSMutableArray *array=[[NSMutableArray alloc] init];
+    for(int i=0;i<10;i++)
+        {
+            [array addObject:[NSString stringWithFormat:@"Item: %d", i+1]];
+        }
+    self.items=array;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,17 +36,25 @@
     // Dispose of any resources that can be recreated.
 }
 - (IBAction)exitMyCasts:(id)sender {
-    [self dismissViewControllerAnimated:NO completion:Nil];
+    [self dismissViewControllerAnimated:YES completion:Nil];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)setItems:(NSArray *)items
+{
+    _items = items;
+    [self.tableView reloadSections:[NSIndexSet indexSetWithIndex:0] withRowAnimation:UITableViewRowAnimationAutomatic];
 }
-*/
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.items.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [[UITableViewCell alloc] init];
+    cell.textLabel.text = self.items[indexPath.row];
+    return cell;
+}
 
 @end
