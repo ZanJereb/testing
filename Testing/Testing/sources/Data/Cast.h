f0e285249d371efbecd48b0db80f8d05655db4b3
ZanJereb <zan@localhost.dlabs.lan>
2016-02-10 15:07:50 +0100

    start of cast class

---
+++ Testing/Testing/sources/Data/Cast.h
@@ -0,0 +1,13 @@
+//
+//  Cast.h
+//  Testing
+//
+//  Created by Zan on 2/10/16.
+//  Copyright © 2016 Zan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Cast : NSObject
+
+@end
---
+++ Testing/Testing/sources/Data/Cast.m
@@ -0,0 +1,21 @@
+//
+//  Cast.m
+//  Testing
+//
+//  Created by Zan on 2/10/16.
+//  Copyright © 2016 Zan. All rights reserved.
+//
+
+#import "Cast.h"
+
+@interface Cast ()
+@property (nonatomic, strong) NSString *title;
+@property (nonatomic, strong) NSString *imageURL;
+@property (nonatomic, strong) NSString *location;
+@property (nonatomic, strong) NSDate *dateCreated;
+
+@end
+
+@implementation Cast
+
+@end
