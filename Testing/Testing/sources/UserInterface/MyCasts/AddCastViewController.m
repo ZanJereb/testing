0f96d8aacbba03b02b973edcdab3701d4619f03b
ZanJereb <zan@localhost.dlabs.lan>
2016-02-12 11:50:16 +0100

    Record a new cast #19

--- Testing/Testing/sources/UserInterface/MyCasts/AddCastViewController.m
+++ Testing/Testing/sources/UserInterface/MyCasts/AddCastViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "AddCastViewController.h"
+@import MediaPlayer;
+@import MobileCoreServices;
 
-@interface AddCastViewController ()
+@interface AddCastViewController ()<UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@property (nonatomic, strong) NSURL *videoPath;
+@property (nonatomic, strong) MPMoviePlayerController *player;
+
 
 @end
 
@@ -18,6 +23,39 @@
 {
     [super viewWillAppear:animated];
     self.navigationController.navigationBar.hidden = NO;
+}
+- (IBAction)startRecording:(id)sender
+{
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera])
+    {
+        UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+        
+        picker.allowsEditing = YES;
+        picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+        picker.mediaTypes = [[NSArray alloc] initWithObjects: (NSString *) kUTTypeMovie, nil];
+        picker.delegate = self;
+        
+        [self presentViewController:picker animated:YES completion:NULL];
+    }
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+    
+}
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info
+{
+    self.videoPath = [info objectForKey:@"UIImagePickerControllerMediaURL"];
+
+}
+
+- (IBAction)previewVideo:(id)sender
+{
+    
+    MPMoviePlayerViewController * controler = [[MPMoviePlayerViewController alloc] initWithContentURL:self.videoPath];
+    
+    [self presentViewController:controler animated:YES completion:nil];
 }
 
 @end
